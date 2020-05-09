44c78e164f0a86c9dfba5e90fba08bdd4b80ef05
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-09 15:06:46 -0400

    updated Listjs to map through list items

--- src/components/List.js
+++ src/components/List.js
@@ -5,7 +5,7 @@ import React, {useState,useEffect} from 'react'
 
 function List({match, location}) {
     const list_id = match.params.id;
-    const [list, setList] = useState({})
+    const [list, setList] = useState({});
 
     const getList = () => {
         fetch(`http://localhost:3000/lists/${list_id}`)
@@ -16,11 +16,25 @@ function List({match, location}) {
 
     useEffect( () => {
         getList();
-    }, []) 
+    },[]) 
 
+    // const list_items = list.list_items
+    // console.log(list.list_items)
+    
     return (
         <>
         <h1>{list.name}</h1>
+        <ul>
+        {list.list_items ?
+            list.list_items.map( item => (
+                <div key={item.id}>
+                    <h2>{item.name}</h2>  
+                </div>
+            ))
+        : <h1>Not Found</h1>}
+        
+        </ul>
+        
         
         </>
     )
